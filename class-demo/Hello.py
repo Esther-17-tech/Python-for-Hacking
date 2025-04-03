b7138161ef1c9bb30b1b2eb97cc298557523f03d
Esther-17-tech <ruthsuccess17@gmail.com>
2025-04-03 07:19:53 -0400

    This is Webdeves class code

---
+++ class-demo/Hello.py
@@ -0,0 +1,34 @@
+# This line is running Python code without a variable
+print('HELLO WORLD')
+print("I am learning python")
+
+
+
+# creating a program with a variable
+first_name= "Esther"
+last_name= "Harrison"
+
+# Concartenation (combining more than variable together)
+print(first_name + " " + last_name) 
+
+
+# pratice on Concartenation using my name and favourite colour
+first_name= "Esther's "
+favourite_colour= "favourite colour is pink."
+print(first_name + favourite_colour)
+
+
+# Types of Variable
+# integer - whole number (eg 0-9)
+# Floating point - decimal number eg (0.1, 0.5)
+# string  - (str) - which are used to stored texts e.g ("Hello", " john", "python")
+# Boolen (bool) - True/False values
+
+# pratice on Concartenation working with age, sentence and country by Mr Ephraim
+age= 18
+sentence= "The legal age to vote in"
+country= "Congo"
+
+full_sentence = sentence + " " + country
+print(full_sentence + " is " + str(age) + ".")
+
